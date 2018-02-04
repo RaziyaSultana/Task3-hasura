918a36aa9ceb7fe3b5b2fec97dad3d45ef595461
RaziyaSultana <raziyasultana1997@gmail.com>
2018-02-04 19:33:12 +0530

    commit one

--- src/components/Notify.js
+++ src/components/Notify.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Alert } from 'react-native';
+import { View, StyleSheet,Alert,Linking } from 'react-native';
 import { Container, Header,Title, Tab, Tabs, TabHeading, Icon, Text } from 'native-base';
 
 
@@ -14,10 +14,27 @@ export default class Notify extends React.Component {
             <Text>Notification</Text>
             <Text>Details saved successfully</Text>
             <Text>Check the link below</Text>
-            <Text>https://docs.google.com/spreadsheets/d/1I2H0-sIvmMh28Qhx1yXwM-8YFoEG-YV5alopHKhngw4/edit?usp=drivesdk
-              </Text>                
+            <Text style={styles.TextStyle} onPress={ ()=> Linking.openURL('https://docs.google.com/spreadsheets/d/1I2H0-sIvmMh28Qhx1yXwM-8YFoEG-YV5alopHKhngw4/edit?usp=drivesdk') }>
+            https://docs.google.com/spreadsheets/d/1I2H0-sIvmMh28Qhx1yXwM-8YFoEG-YV5alopHKhngw4/edit?usp=drivesdk</Text>
+            
       </Container>
     );
   }
 }
 
+
+const styles = StyleSheet.create({
+ 
+  MainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+ 
+  TextStyle: {
+ 
+    color: '#E91E63',
+    textDecorationLine: 'underline'
+ 
+  }
+});
